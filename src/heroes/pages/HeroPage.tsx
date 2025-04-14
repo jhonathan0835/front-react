d0801eb3ed3b196234f77b3383b0a48c3959d428
jrodriguezra <jrodriguezra@indracompany.com>
2025-04-14 18:27:35 -0500

    leer los parametros enviados y con eso mostrar imagen en otra pantalla

--- src/heroes/pages/HeroPage.tsx
+++ src/heroes/pages/HeroPage.tsx
@@ -1,11 +1,15 @@
-import { Navigate, useParams } from "react-router-dom"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 import getHeroesById from "../helpers/getHeroesHeroById";
 
 const HeroPage = () => {
 
   const { id } = useParams();
   const hero = getHeroesById(id);
+  const navigate = useNavigate();
 
+  const onNavigateBack = () => {
+     navigate(-1);
+  }
   if (!id) {
     return <Navigate to="/marvel" />;
   }
@@ -15,7 +19,30 @@ const HeroPage = () => {
   }
   console.log(id);
   return (
-      <h1>{hero.superhero}</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+        src={ `/public/assets/${id}.jpg`}
+        alt={hero.superhero} 
+        className="img-thumbnail" />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego:</b>{hero.alter_ego}</li>
+          <li className="list-group-item"><b>publisher:</b>{hero.publisher}</li>
+          <li className="list-group-item"><b>first_appearance:</b>{hero.first_appearance}</li>
+
+          <h5 className="mt-3">characters </h5>
+          <p>{hero.characters}</p>
+          <button
+            className="btn btn-outline-primary"
+            onClick={onNavigateBack}> Volver </button>      
+
+        </ul>
+      </div>
+
+    </div>  
   )
 }
 
