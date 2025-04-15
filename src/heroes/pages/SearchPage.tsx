884a69060501945f073b695f316ccc600bc77790
jrodriguezra <jrodriguezra@indracompany.com>
2025-04-14 23:00:08 -0500

    Diseñando la pagina de busquedad

--- src/heroes/pages/SearchPage.tsx
+++ src/heroes/pages/SearchPage.tsx
@@ -4,7 +4,42 @@ const SearchPage = () => {
   return (
     <>
       <h1>Search Page</h1>
-      <p>This is the search page.</p>
+      <hr />
+      <div className="row"> 
+      <div className='col-5'>
+        <h4>searching</h4>
+        <hr />
+        <form>
+          <input 
+          type='text'
+          placeholder ='search hero'
+          className='form-control'
+          name='searchText'
+          autoComplete='off'
+          />
+          <button
+          className='btn btn-outline-primary mt-1'
+          type='submit'
+          >
+            Search
+          </button>
+        </form>
+
+      </div>
+      
+      <div className='col-7'>
+        <h4>Results</h4>
+        <hr />
+        <div className='alert alert-primary'>
+          Search a hero
+        </div>
+        <div className='alert alert-danger'>
+          There's no results
+        </div>
+      </div>  
+      </div>
+
+
     </>
   )
 }
